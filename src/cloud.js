7df7d008ec5adef7f840db692f0432b258263d1e
hxiong <hxiong@ctrip.com>
2024-10-07 06:32:33 +0800

    [fix]优化发件计数

--- src/cloud.js
+++ src/cloud.js
@@ -5,25 +5,30 @@ const request = require('request');
 
 // 将此comment通知给被 @ 的人
 function sendReplyNotification(currentComment) {
-    // AT评论通知
-    let pid = currentComment.get('pid');
+    return new Promise((resolve, reject)=>{
+        // AT评论通知
+        let pid = currentComment.get('pid');
 
-    if (!pid) {
-        console.log("这条评论没有 @ 任何人");
-        return;
-    }
-
-    // 通过被 @ 的评论 id, 则找到这条评论留下的邮箱并发送通知.
-    let query = new AV.Query('Comment');
-    query.get(pid).then(function (parentComment) {
-        if (parentComment.get('mail')) {
-            mailService.send(currentComment, parentComment);
-        } else {
-            console.log(currentComment.get('nick') + " @ 了" + parentComment.get('nick') + ", 但被 @ 的人没留邮箱... 无法通知");
+        if (!pid) {
+            console.log("这条评论没有 @ 任何人");
+            resolve(1);
+            return;
         }
-    }, function (error) {
-        console.warn('好像 @ 了一个不存在的人!!!', error && error.message);
-    });
+
+        // 通过被 @ 的评论 id, 则找到这条评论留下的邮箱并发送通知.
+        let query = new AV.Query('Comment');
+        query.get(pid).then(function (parentComment) {
+            if (parentComment.get('mail')) {
+                mailService.send(currentComment, parentComment);
+            } else {
+                console.log(currentComment.get('nick') + " @ 了" + parentComment.get('nick') + ", 但被 @ 的人没留邮箱... 无法通知");
+            }
+            resolve(1);
+        }, function (error) {
+            console.warn('好像 @ 了一个不存在的人!!!', error && error.message);
+            resolve(0);
+        });
+    })
 }
 
 AV.Cloud.afterSave('Comment', function (request) {
@@ -41,15 +46,17 @@ AV.Cloud.define('resend_mails', function(req) {
     query.notEqualTo('isNotified', true);
     // 如果你的评论量很大，可以适当调高数量限制，最高1000
     query.limit(200);
-    return query.find().then(function(results) {
-        new Promise((resolve, reject)=>{
-            const count = results.length;
-            for (var i = 0; i < results.length; i++ ) {
-                sendReplyNotification(results[i]);
+    return query.find().then(function(commentList) {
+        return new Promise(async (resolve)=>{
+            const totalCount = commentList.length;
+            let successCount = 0;
+            for (comment of commentList) {
+                const sendCount = await sendReplyNotification(comment);
+                successCount += sendCount;
             }
-            resolve(count);
-        }).then((count)=>{
-            console.log(`昨日${count}条未成功发送的通知邮件处理完毕！`);
+            resolve({ totalCount, successCount });
+        }).then(({ totalCount, successCount })=>{
+            console.log(`昨日${totalCount}条未成功发送的通知邮件，现已成功处理${successCount}条！`);
         }).catch((error)=>{
             console.log('error ~ ', error && error.message);
         });
